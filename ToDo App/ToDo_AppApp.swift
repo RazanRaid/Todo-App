a803670c5a372ba5c8e07aac932ab62273c860c2
Razan Raid <razanraid990@gmail.com>
2023-08-19 20:24:22 +0400

    Initial Commit

---
+++ ToDo App/ToDo_AppApp.swift
@@ -0,0 +1,17 @@
+//
+//  ToDo_AppApp.swift
+//  ToDo App
+//
+//  Created by Razan Raid on 19/08/2023.
+//
+
+import SwiftUI
+
+@main
+struct ToDo_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
